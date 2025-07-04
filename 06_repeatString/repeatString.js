ab0755cf50832e407eb9d4468e85a1e3c7ea7cb0
Muhammad Awais <mawais08901@gmail.com>
2025-07-04 12:31:23 +0500

    RepeatString Exercise
    i completed this Exercise with the help of readme and
    the hints provided to me and passed every test.
    
    Changes to be committed:
    modified:   repeatString.js
    modified:   repeatString.spec.js

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+
+    if (num < 0) {
+        return "ERROR";
+    }
+    else {
+        let result = "";
+
+        for (let i = 1; i <= num; i++) {
+            result += string;
+        }
+
+        return result;
+    }
 
 };
 
-// Do not edit below this line
 module.exports = repeatString;
