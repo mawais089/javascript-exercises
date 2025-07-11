9090a4642b88402689616ade2eeceb28acd53d61
Muhammad Awais <mawais08901@gmail.com>
2025-07-11 17:04:46 +0500

    fibonacci Exercise
    i completed this Exercise with the help of google
    searching for what is fibonacci sequence and how
    to solve it then i used my problem solving skills
    to solve it in javascript.
    
    Changes to be committed:
    modified:   fibonacci.js
    modified:   fibonacci.spec.js

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
 
+    let count;
+
+    if (num < 0) return "OOPS";
+
+    if (typeof num !== 'number') {
+        count = parseInt(num);
+    }
+    else{
+        count = num;
+    }
+
+    let fib = [0,1];
+
+    for (let i = 2; i <= count; i++){
+        fib[i] = fib[i - 1] + fib [i - 2]
+    }
+    return fib[count];
 };
 
 // Do not edit below this line
