806f4d3b743e39ffbfc5b80713131447639481e8
Muhammad Awais <mawais08901@gmail.com>
2025-07-11 12:10:46 +0500

    Calculator Exercise
    I completed this Exercise and passed all of my tests.
    i was little bit confused in factorial and the power
    function so i googled for them like what is factorial
    and how can i power one number to another number.
    
    Changes to be committed:
    modified:   calculator.js
    modified:   calculator.spec.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b ;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((prev,current) => prev + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prev,current) => prev * current, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+
+  let result = 1;
+
+  for(let i = 1; i <= num; i++){
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
