a4e65c0373b8e0fb98c6ad038c6a735c64816856
Muhammad Awais <mawais08901@gmail.com>
2025-07-11 12:38:55 +0500

    Palindromes Exercise
    This exercise was little difficult for me so i checked
    the solution folder only for i was confused in the
    alphanumericals part.
    
    Changes to be committed:
    modified:   palindromes.js
    modified:   palindromes.spec.js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+    const aplphanumericals = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    const cleanedString = str.
+                        toLowerCase().
+                        split('').
+                        filter(char => aplphanumericals.includes(char)).
+                        join('');
 
+    const reversedStr = cleanedString.split('').reverse().join('');
+
+    return reversedStr === cleanedString;
 };
 
 // Do not edit below this line
